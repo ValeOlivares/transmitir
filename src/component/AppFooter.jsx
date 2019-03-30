8d1a5bf46f9a3fb4b9af1e9a8fb786fd3ecb2eaa
Valentina Neira <valentinaolivares85@gmail.com>
2019-03-30 12:46:51 -0300

    add footer

---
+++ src/component/AppFooter.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Footer from 'react-materialize'
+
+
+class AppFooter extends Component {
+  render() {
+    return (
+      <Footer copyrights="&copy 2015 Copyright Text" moreLinks={<a />} links={<ul />} className="example">
+        <h5 className="white-text">
+          Footer Content
+        </h5>
+        <p className="grey-text text-lighten-4">
+          You can use rows and columns here to organize your footer content.
+        </p>
+      </Footer>
+    )
+  }
+}
+
+export default AppFooter; 
