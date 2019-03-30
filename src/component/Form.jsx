c23f7b5f6754efdcd64e31c4a12bf69acc4866c2
Ire <ijrodriguezpa@sodimac.cl>
2019-03-30 13:34:52 -0300

    mic working

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Curricular from './Curricular';
+import { TextInput } from 'react-materialize';
+import AudioAnalyser from './AudioAnalyser';
 
 class Form extends Component {
     constructor(title) {
@@ -9,6 +11,7 @@ class Form extends Component {
             formCounter: 1
         }
         this.handleClick = this.handleClick.bind(this);
+        this.toggleMicrophone = this.toggleMicrophone.bind(this);
     }
 
     handleClick(event) {
@@ -22,8 +25,29 @@ class Form extends Component {
 
         this.setState({
             formCounter: add,
-        //   isValid: null
+            //   isValid: null
         });
+    }
+
+    async getMicrophone() {
+        const audio = await navigator.mediaDevices.getUserMedia({
+            audio: true,
+            video: false
+        });
+        this.setState({ audio });
+    }
+
+    stopMicrophone() {
+        this.state.audio.getTracks().forEach(track => track.stop());
+        this.setState({ audio: null });
+    }
+
+    toggleMicrophone() {
+        if (this.state.audio) {
+            this.stopMicrophone();
+        } else {
+            this.getMicrophone();
+        }
     }
 
     render() {
@@ -40,18 +64,34 @@ class Form extends Component {
                 {formCounter === 1 ? (
                     <div>
                         <h3>Nombre</h3>
+                        <TextInput value={title} />
+
                         <button onClick={this.handleClick.bind(this)}>Next</button>
                     </div>
                 ) : null}
                 {formCounter === 2 ? (
                     <div>
                         <h3>Tono</h3>
-                        <button onClick={this.handleClick.bind(this)}>Next</button>
+
+                        <button onClick={this.handleClick.bind(this)}>Agudo</button>
+                        <button onClick={this.handleClick.bind(this)}>Grave</button>
                     </div>
                 ) : null}
                 {formCounter === 3 ? (
                     <div>
                         <h3>Registro</h3>
+                        <div className="controls">
+                            <button onClick={this.toggleMicrophone}>
+                                {this.state.audio ? 'Stop microphone' : 'Get microphone input'}
+                            </button>
+                        </div>
+                        {this.state.audio ? <AudioAnalyser audio={this.state.audio} /> : ''}
+
+
+
+
+
+
                         <button onClick={this.handleClick.bind(this)}>Next</button>
                     </div>
                 ) : null}
@@ -64,8 +104,8 @@ class Form extends Component {
                 {formCounter === 5 ? (
                     <div>
                         <h3>Malla</h3>
-                        <Curricular/>
-                        
+                        <Curricular />
+
                     </div>
                 ) : null}
             </div>
